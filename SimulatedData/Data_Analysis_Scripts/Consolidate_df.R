5ab24242f9487b93846443b60a66f3258aff9143
RDorney <ryley.dorney@my.jcu.edu.au>
2026-01-08 14:24:08 +1000

    Finish CTAT-LR integration, re-organise main RMD

---
+++ SimulatedData/Data_Analysis_Scripts/Consolidate_df.R
@@ -0,0 +1,53 @@
+#Consolidate data frames
+
+dLGF<- Annot_LongGF_Sim[c(1:24, 34, 41:43)] %>% 
+  unique()%>% #remove duplicate values
+  select(c(5, 13, 14, 25:28))%>%#select only columns needed for downstream analysis
+  rename(read_supp = V3.x)
+
+dJF <- Annot_JAFFAL_Sim[c(1:25, 43, 50:52)] %>% 
+  unique()%>% #remove duplicate values
+  select(c(13, 22, 23, 26:29)) %>% #select only columns needed for downstream analysis
+  rename(read_supp = spanning.reads)
+
+dJF3 <- Annot_JAFFAL_3Gene_Sim[c(1:21, 31, 38:40)] %>%
+  unique()%>% #remove duplicate values
+  select(c(5, 10, 11, 22:25))%>% #extract only columns needed for downstream analysis
+  rename(read_supp = Reads)
+
+dGen <- Annot_Genion_Sim[c(1:27, 37, 44:46)]%>%
+  unique()%>% #remove duplicate values
+  select(c(1, 11, 19, 28:31))%>% #extract only columns needed for downstream analysis
+  rename(read_supp = V5,
+         fusion.gene.id = V1)
+
+dFS <- Annot_FusionSeeker_Sim[c(1:28, 38, 45:47)]%>%
+  unique()%>% 
+  select(c(8,15, 16, 29:32))%>%
+  rename(read_supp = NumSupp)
+
+dCTAT_LR<- Annot_CTATLR_Sim[c(1:25, 43, 50:52)] %>% 
+  unique()%>% #remove duplicate values
+  select(c(2, 22, 23, 26:29))%>%#select only columns needed for downstream analysis
+  rename(read_supp = num_LR)
+
+combined_data <- rbind(dLGF, dJF, dJF3, dGen, dFS, dCTAT_LR)
+combined_data$read_supp <- as.numeric(combined_data$read_supp)
+
+write_tsv(combined_data, paste0(Analysis_output_folder, "combined_data_FusionCalls.tsv"))
+
+combined_data_annotation <- filter(combined_data, read_supp >= 2) %>% unique() %>% mutate(recall_category = case_when(
+  grepl("false", fusionType) ~ "False_Call",
+  grepl("truncated|reverse|chromosomal_misalignment|wrong", fusionType) ~ "Partial_Recall",
+  TRUE ~ "True_Recall"))%>%
+  group_by(control, depth, Sequence_Identity, Algorithm, recall_category) %>%
+  summarise(RECALL_COUNT = n()) %>%
+  summarise(False_Call_Number = sum(RECALL_COUNT[recall_category == "False_Call"], na.rm = TRUE),
+            Partial_Call_Number = sum(RECALL_COUNT[recall_category == "Partial_Recall"], na.rm = TRUE),
+            True_Call_Number = sum(RECALL_COUNT[recall_category == "True_Recall"], na.rm = TRUE))%>% 
+  ungroup() %>% 
+  complete(control, depth, Sequence_Identity, Algorithm, fill = list(False_Call_Number = 0, Partial_Call_Number = 0, True_Call_Number = 0))
+combined_data_annotation$depth <- factor(combined_data_annotation$depth, levels = c("1GB", "10GB", "100GB"))
+combined_data_annotation$Sequence_Identity <- factor(combined_data_annotation$Sequence_Identity, levels = c("85%", "90%", "95%"))
+
+write_tsv(combined_data_annotation, file=paste0(Analysis_output_folder, "combined_data_annotation.tsv"))
--- SimulatedData/Data_Analysis_Scripts/PreprocessCTAT-LR.R
+++ SimulatedData/Data_Analysis_Scripts/PreprocessCTAT-LR.R
@@ -35,37 +35,39 @@ CTATLR_SIM_a2 <- CTATLR_SIM_a1 %>%
   mutate(Gene2_ensembl_ID= coalesce(ensembl_gene_id, RightGene)) %>% select(-ensembl_gene_id) %>%
   unite("fusion.gene.id" , c(Gene1_ensembl_ID, Gene2_ensembl_ID), sep = ":", remove= FALSE)
    
-Annot_CTATLR_Sim <- CTATLR_SIM_a2 %>% left_join(Simulated_Fusion_Info_2, by = 'fusion.gene.id') 
+Annot_CTATLR_Sim <- CTATLR_SIM_a2 %>% 
+  left_join(Simulated_Fusion_Info_2, by = 'fusion.gene.id', relationship = "many-to-many") 
 
-Annot_CTATLR_Sim$fusionType <- mapply(function(g1, g2, Gen1, Gen2, current_type, chr1, chr2) {
+Annot_CTATLR_Sim$fusionType <- mapply(function(g1, g2, current_type, chr1, chr2, gene_name1, gene_name2) {
   # Check if the current fusionType is empty
   if (current_type == "" || is.na(current_type)) {
+    #check for truncated_tri_fusions
     if (any(str_detect(subset(Simulated_Fusion_Info_2, fusionType == "tri_fusion")$fusion.gene.id, paste0(g1, ":", g2)) |
             str_detect(paste(subset(Simulated_Fusion_Info_2, fusionType == "tri_fusion")$V1, subset(Simulated_Fusion_Info_2, fusionType == "tri_fusion")$V3, sep = ":"), paste0(g1, ":", g2)))) {
       return("truncated_tri_fusion")
-    } else if (any(str_detect(subset(Simulated_Fusion_Info_2, fusionType == "tri_fusion")$fusion.gene.id, paste0(g2, ":", g1)) |
+      #check for reverse order fusions
+    } else if (any(str_detect(subset(Simulated_Fusion_Info_2, fusionType == "tri_fusion")$fusion.gene.id, paste0(g2, ":", g1))|
                    str_detect(paste(subset(Simulated_Fusion_Info_2, fusionType == "tri_fusion")$V1, subset(Simulated_Fusion_Info_2, fusionType == "tri_fusion")$V3, sep = ":"), paste0(g2, ":", g1)))){
       return("reverse_order:truncated_tri_fusion") 
     } else if (any(str_detect(subset(Simulated_Fusion_Info_2, fusionType != "tri_fusion")$fusion.gene.id, paste0(g2, ":", g1)))){
       matching_row <- subset(Simulated_Fusion_Info_2, fusionType != "tri_fusion")$fusionType[which(str_detect(subset(Simulated_Fusion_Info_2, fusionType != "tri_fusion")$fusion.gene.id, paste0(g2, ":", g1)))]
       return(paste("reverse_order:", matching_row[1])) 
+      #check for chromosomal misalignment
     } else if (any(str_detect(subset(Simulated_Fusion_Info_2, fusionType != "tri_fusion")$original.fusion.gene.id, paste0(g1, ":", g2)))){
       matching_row <- subset(Simulated_Fusion_Info_2, fusionType != "tri_fusion")$fusionType[which(str_detect(subset(Simulated_Fusion_Info_2, fusionType != "tri_fusion")$original.fusion.gene.id, paste0(g1, ":", g2)))]
-      return(paste0("chromosomal_misalignment:", matching_row[1])) 
-    } else if ((grepl("chrM", chr1, ignore.case = TRUE) & !grepl("chrM", chr2, ignore.case = TRUE)) | (!grepl("chrM", chr1, ignore.case = TRUE) & grepl("chrM", chr2, ignore.case = TRUE))){
+      return(paste("chromosomal_misalignment:", matching_row[1])) 
+      #check for false fusions
+    } else if ((grepl("chrM:", chr1, ignore.case = TRUE) & !grepl("chrM:", chr2, ignore.case = TRUE)) | (!grepl("chrM:", chr1, ignore.case = TRUE) & grepl("chrM:", chr2, ignore.case = TRUE))){
       return("false_fusion:mitochondrial_genomic") 
-    }else if (grepl("chrM", chr1, ignore.case = TRUE) & grepl("chrM", chr2, ignore.case = TRUE)){
+    }else if (grepl("chrM:", chr1, ignore.case = TRUE) & grepl("chrM:", chr2, ignore.case = TRUE)){
       return("false_fusion:mitochondrial") 
-    }else if (((g1 == g2)|(Gen1 == Gen2)) & (chr1 != chr2)){
+    }else if ((gene_name1 == gene_name2) & (chr1 != chr2)){
       return("false_fusion:self_misalignment") 
     }else {
       return("false_fusion")
     }
   }
   return(current_type)
-}, Annot_CTATLR_Sim$Gene1_ensembl_ID, Annot_CTATLR_Sim$Gene2_ensembl_ID,
-Annot_CTATLR_Sim$LeftGene, Annot_CTATLR_Sim$RightGene, 
-Annot_CTATLR_Sim$fusionType, Annot_CTATLR_Sim$chrom1, Annot_CTATLR_Sim$chrom2)
+}, Annot_CTATLR_Sim$Gene1_ensembl_ID, Annot_CTATLR_Sim$Gene2_ensembl_ID, Annot_CTATLR_Sim$fusionType, Annot_CTATLR_Sim$chrom1, Annot_CTATLR_Sim$chrom2, Annot_CTATLR_Sim$LeftGene, Annot_CTATLR_Sim$RightGene)
 
-setdiff(unique(c(ogCTATLR$ID, ogCTATLR$Source)), unique(c(Annot_CTATLR_Sim$ID, Annot_CTATLR_Sim$Source)))
 
