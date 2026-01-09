997c86793d3304aa92c55e3166d10e8faf5ceb75
RDorney <ryley.dorney@my.jcu.edu.au>
2026-01-09 15:07:10 +1000

    Add reference to resource usage script in main mark down document

---
+++ CellLineData/LongGF_extractSumGF.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+grep SumGF LongGF.run.SGNex_K562_directcDNA_replicate_combined.log > LongGF.SGNex_K562_directcDNA_replicate_combined.tsv
+grep SumGF LongGF.run.SGNex_K562_directcDNA_1G_rasusa.log > LongGF.SGNex_K562_directcDNA_1G_rasusa.tsv
+grep SumGF LongGF.run.SGNex_K562_directcDNA_2.5G_rasusa.log > LongGF.SGNex_K562_directcDNA_2.5G_rasusa.tsv
+grep SumGF LongGF.run.SGNex_K562_directcDNA_5G_rasusa.log > LongGF.SGNex_K562_directcDNA_5G_rasusa.tsv
+
+grep SumGF LongGF.run.SGNex_K562_cDNA_combined.log > LongGF.SGNex_K562_cDNA_combined.tsv
+grep SumGF LongGF.run.SGNex_K562_PCRcDNA_combined_10G_rasusa.log > LongGF.SGNex_K562_PCRcDNA_combined_10G_rasusa.tsv
+grep SumGF LongGF.run.SGNex_K562_PCRcDNA_combined_1G_rasusa.log > LongGF.SGNex_K562_PCRcDNA_combined_1G_rasusa.tsv
+grep SumGF LongGF.run.SGNex_K562_PCRcDNA_combined_2.5G_rasusa.log > LongGF.SGNex_K562_PCRcDNA_combined_2.5G_rasusa.tsv
+grep SumGF LongGF.run.SGNex_K562_PCRcDNA_combined_5G_rasusa.log > LongGF.SGNex_K562_PCRcDNA_combined_5G_rasusa.tsv
+grep SumGF LongGF.run.SGNex_K562_PCRcDNA_combined_7.5G_rasusa.log > LongGF.SGNex_K562_PCRcDNA_combined_7.5G_rasusa.tsv
+
+grep SumGF LongGF.run.SGNex_K562_directRNA_replicate_combined.log > LongGF.SGNex_K562_directRNA_replicate_combined.tsv
+grep SumGF LongGF.run.SGNex_K562_directRNA_1G_rasusa.log > LongGF.SGNex_K562_directRNA_1G_rasusa.tsv
+grep SumGF LongGF.run.SGNex_K562_directRNA_2.5G_rasusa.log > LongGF.SGNex_K562_directRNA_2.5G_rasusa.tsv
+
+grep SumGF LongGF.run.SGNex_MCF7_directcDNA_concatenated.log > LongGF.SGNex_MCF7_directcDNA_concatenated.tsv
+grep SumGF LongGF.run.SGNex_MCF7_directcDNA_1G_rasusa.log > LongGF.SGNex_MCF7_directcDNA_1G_rasusa.tsv
+grep SumGF LongGF.run.SGNex_MCF7_directcDNA_2.5G_rasusa.log > LongGF.SGNex_MCF7_directcDNA_2.5G_rasusa.tsv
+grep SumGF LongGF.run.SGNex_MCF7_directcDNA_5G_rasusa.log > LongGF.SGNex_MCF7_directcDNA_5G_rasusa.tsv
+grep SumGF LongGF.run.SGNex_MCF7_directcDNA_7.5G_rasusa.log > LongGF.SGNex_MCF7_directcDNA_7.5G_rasusa.tsv
+
+grep SumGF LongGF.run.SGNex_MCF7_cDNA_combined.log > LongGF.SGNex_MCF7_cDNA_combined.tsv
+grep SumGF LongGF.run.SGNex_MCF7_PCRcDNA_combined_1G_rasusa.log > LongGF.SGNex_MCF7_PCRcDNA_combined_1G_rasusa.tsv
+grep SumGF LongGF.run.SGNex_MCF7_PCRcDNA_combined_10G_rasusa.log > LongGF.SGNex_MCF7_PCRcDNA_combined_10G_rasusa.tsv
+grep SumGF LongGF.run.SGNex_MCF7_PCRcDNA_combined_5G_rasusa.log > LongGF.SGNex_MCF7_PCRcDNA_combined_5G_rasusa.tsv
+grep SumGF LongGF.run.SGNex_MCF7_PCRcDNA_combined_2.5G_rasusa.log > LongGF.SGNex_MCF7_PCRcDNA_combined_2.5G_rasusa.tsv
+grep SumGF LongGF.run.SGNex_MCF7_PCRcDNA_combined_7.5G_rasusa.log > LongGF.SGNex_MCF7_PCRcDNA_combined_7.5G_rasusa.tsv
+
+grep SumGF LongGF.run.SGNex_MCF7_directRNA_replicate_concatenated.log > LongGF.SGNex_MCF7_directRNA_replicate_concatenated.tsv
+grep SumGF LongGF.run.SGNex_MCF7_directRNA_1G_rasusa.log > LongGF.SGNex_MCF7_directRNA_1G_rasusa.tsv
+grep SumGF LongGF.run.SGNex_MCF7_directRNA_5G_rasusa.log > LongGF.SGNex_MCF7_directRNA_5G_rasusa.tsv
+grep SumGF LongGF.run.SGNex_MCF7_directRNA_2.5G_rasusa.log > LongGF.SGNex_MCF7_directRNA_2.5G_rasusa.tsv
+grep SumGF LongGF.run.SGNex_MCF7_directRNA_7.5G_rasusa.log > LongGF.SGNex_MCF7_directRNA_7.5G_rasusa.tsv
