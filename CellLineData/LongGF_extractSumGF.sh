9d5bc503c59ce367359e78cecd299ec49638915e
RDorney <ryley.dorney@my.jcu.edu.au>
2026-01-23 13:05:44 +1000

    updating to Gencode 44 output

--- CellLineData/LongGF_extractSumGF.sh
+++ CellLineData/LongGF_extractSumGF.sh
@@ -1,36 +1,18 @@
 #!/bin/bash
+set -euo pipefail
 
-grep SumGF LongGF.run.SGNex_K562_directcDNA_replicate_combined.log > LongGF.SGNex_K562_directcDNA_replicate_combined.tsv
-grep SumGF LongGF.run.SGNex_K562_directcDNA_1G_rasusa.log > LongGF.SGNex_K562_directcDNA_1G_rasusa.tsv
-grep SumGF LongGF.run.SGNex_K562_directcDNA_2.5G_rasusa.log > LongGF.SGNex_K562_directcDNA_2.5G_rasusa.tsv
-grep SumGF LongGF.run.SGNex_K562_directcDNA_5G_rasusa.log > LongGF.SGNex_K562_directcDNA_5G_rasusa.tsv
+DIR="/bioinformatics/ryley/Gencode44/SGNex_Fusionv44/LongGF"
+LONGGF_SGNEX="LongGF.run.SGNex"
+for file in "$DIR"/"${LONGGF_SGNEX}"*.log; do
+    [[ -f "$file" ]] || continue
 
-grep SumGF LongGF.run.SGNex_K562_cDNA_combined.log > LongGF.SGNex_K562_cDNA_combined.tsv
-grep SumGF LongGF.run.SGNex_K562_PCRcDNA_combined_10G_rasusa.log > LongGF.SGNex_K562_PCRcDNA_combined_10G_rasusa.tsv
-grep SumGF LongGF.run.SGNex_K562_PCRcDNA_combined_1G_rasusa.log > LongGF.SGNex_K562_PCRcDNA_combined_1G_rasusa.tsv
-grep SumGF LongGF.run.SGNex_K562_PCRcDNA_combined_2.5G_rasusa.log > LongGF.SGNex_K562_PCRcDNA_combined_2.5G_rasusa.tsv
-grep SumGF LongGF.run.SGNex_K562_PCRcDNA_combined_5G_rasusa.log > LongGF.SGNex_K562_PCRcDNA_combined_5G_rasusa.tsv
-grep SumGF LongGF.run.SGNex_K562_PCRcDNA_combined_7.5G_rasusa.log > LongGF.SGNex_K562_PCRcDNA_combined_7.5G_rasusa.tsv
-
-grep SumGF LongGF.run.SGNex_K562_directRNA_replicate_combined.log > LongGF.SGNex_K562_directRNA_replicate_combined.tsv
-grep SumGF LongGF.run.SGNex_K562_directRNA_1G_rasusa.log > LongGF.SGNex_K562_directRNA_1G_rasusa.tsv
-grep SumGF LongGF.run.SGNex_K562_directRNA_2.5G_rasusa.log > LongGF.SGNex_K562_directRNA_2.5G_rasusa.tsv
-
-grep SumGF LongGF.run.SGNex_MCF7_directcDNA_concatenated.log > LongGF.SGNex_MCF7_directcDNA_concatenated.tsv
-grep SumGF LongGF.run.SGNex_MCF7_directcDNA_1G_rasusa.log > LongGF.SGNex_MCF7_directcDNA_1G_rasusa.tsv
-grep SumGF LongGF.run.SGNex_MCF7_directcDNA_2.5G_rasusa.log > LongGF.SGNex_MCF7_directcDNA_2.5G_rasusa.tsv
-grep SumGF LongGF.run.SGNex_MCF7_directcDNA_5G_rasusa.log > LongGF.SGNex_MCF7_directcDNA_5G_rasusa.tsv
-grep SumGF LongGF.run.SGNex_MCF7_directcDNA_7.5G_rasusa.log > LongGF.SGNex_MCF7_directcDNA_7.5G_rasusa.tsv
-
-grep SumGF LongGF.run.SGNex_MCF7_cDNA_combined.log > LongGF.SGNex_MCF7_cDNA_combined.tsv
-grep SumGF LongGF.run.SGNex_MCF7_PCRcDNA_combined_1G_rasusa.log > LongGF.SGNex_MCF7_PCRcDNA_combined_1G_rasusa.tsv
-grep SumGF LongGF.run.SGNex_MCF7_PCRcDNA_combined_10G_rasusa.log > LongGF.SGNex_MCF7_PCRcDNA_combined_10G_rasusa.tsv
-grep SumGF LongGF.run.SGNex_MCF7_PCRcDNA_combined_5G_rasusa.log > LongGF.SGNex_MCF7_PCRcDNA_combined_5G_rasusa.tsv
-grep SumGF LongGF.run.SGNex_MCF7_PCRcDNA_combined_2.5G_rasusa.log > LongGF.SGNex_MCF7_PCRcDNA_combined_2.5G_rasusa.tsv
-grep SumGF LongGF.run.SGNex_MCF7_PCRcDNA_combined_7.5G_rasusa.log > LongGF.SGNex_MCF7_PCRcDNA_combined_7.5G_rasusa.tsv
-
-grep SumGF LongGF.run.SGNex_MCF7_directRNA_replicate_concatenated.log > LongGF.SGNex_MCF7_directRNA_replicate_concatenated.tsv
-grep SumGF LongGF.run.SGNex_MCF7_directRNA_1G_rasusa.log > LongGF.SGNex_MCF7_directRNA_1G_rasusa.tsv
-grep SumGF LongGF.run.SGNex_MCF7_directRNA_5G_rasusa.log > LongGF.SGNex_MCF7_directRNA_5G_rasusa.tsv
-grep SumGF LongGF.run.SGNex_MCF7_directRNA_2.5G_rasusa.log > LongGF.SGNex_MCF7_directRNA_2.5G_rasusa.tsv
-grep SumGF LongGF.run.SGNex_MCF7_directRNA_7.5G_rasusa.log > LongGF.SGNex_MCF7_directRNA_7.5G_rasusa.tsv
+    #Output description
+    output_prefix=$(basename ${file} .log )
+    # Skip if output already exists
+    if [[ -f "${DIR}/${output_prefix}.tsv" ]]; then
+        echo "[SKIP] ${output_prefix}.tsv already exists"
+        continue
+    fi
+    
+    grep "SumGF" ${file} > "${DIR}/${output_prefix}.tsv"
+done
