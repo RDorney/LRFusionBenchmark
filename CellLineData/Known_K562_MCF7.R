a3b97fbbb79af319d0c4e0d4566f39daf3f54452
RDorney <ryley.dorney@my.jcu.edu.au>
2026-02-08 22:27:18 +1000

    updating known list

--- CellLineData/Known_K562_MCF7.R
+++ CellLineData/Known_K562_MCF7.R
@@ -1,9 +1,16 @@
 Cell_line_data_directory <- "~/LongReadFusionCallerBenchmark/CellLineData/"
 
 known_fusions <- read.table(paste0(Cell_line_data_directory, "real_gene_fusions.tsv"), header=TRUE)
+
+################################################################
+# Load Biomart
+#
+# if biomart doesn't work, proceed below to use annotation hub
+################################################################
+options(timeout = 300)  # 5 minutes
 ensemblv110 <- useEnsembl("ensembl", 
                           dataset = "hsapiens_gene_ensembl", 
-                          version = 110)
+                          host = "https://jul2023.archive.ensembl.org")
 gene_info <- distinct(rbind(getBM(attributes = c("external_gene_name", "external_gene_name", "ensembl_gene_id"),
                    filters = "external_gene_name",
                    values = unique(c(known_fusions$Gene1, known_fusions$Gene2)),
@@ -15,21 +22,68 @@ gene_info <- distinct(rbind(getBM(attributes = c("external_gene_name", "external
 known_fusions_biomart <- left_join(known_fusions,  gene_info, 
                                    by  = c("Gene1"="external_gene_name")) %>% 
   left_join(gene_info, by  = c("Gene2"="external_gene_name"))
-write_csv(known_fusions_biomart, file="~/LongReadFusionCallerBenchmark/CellLineData/known_fusions_biomart.csv")
+write_tsv(known_fusions_biomart, file="~/LongReadFusionCallerBenchmark/CellLineData/known_fusions_biomart.tsv")
 
-known_fusions_manual_annotation <- read_excel("~/LongReadFusionCallerBenchmark/CellLineData/known_fusions_manual_annotation.xlsx")
+###########################################################
+# If Biomart did not work,
+# use AnnotationHub()
+###########################################################
+ah <- AnnotationHub()
+query(ah, c("EnsDb", "Hsapiens", "110"))
+ensembldbv110 <- ah[["AH113665"]]
+query(ah, c("EnsDb", "Hsapiens", "109"))
+ensmebldbv109 <- ah[["AH109606"]]
 
-gene_info <- getBM(attributes = c("external_gene_name", "ensembl_gene_id"),
-                   filters = "ensembl_gene_id",
-                   values = unique(c(known_fusions_manual_annotation$ensembl_gene_id.x, known_fusions_manual_annotation$ensembl_gene_id.y)),
-                   mart = ensemblv110) %>% unique()
+latest_human <- query(ah, c("EnsDb", "Hsapiens"))
+edb_latest <- latest_human[[max(names(latest_human))]]
+
+# Step 1: Map your old synonyms to IDs using the LATEST version
+# (Ensembl's GENENAME often contains synonyms in its search index)
+gene_name_check<- checkGeneSymbols(unique(c(known_fusions$Gene1, known_fusions$Gene2)),
+                                   species = "human") %>%
+  mutate(
+    Suggested.Symbol = coalesce(Suggested.Symbol, x)
+  )
+modern_mapping <- ensembldb::select(edb_latest, 
+                                    keys = unique(c(known_fusions$Gene1, known_fusions$Gene2, gene_name_check$Suggested.Symbol)), 
+                                    keytype = "GENENAME", 
+                                    columns = c("GENENAME", "GENEID", "SYMBOL"))%>%
+  distinct(SYMBOL, .keep_all = TRUE)
+# Step 2: Use those IDs to find what v110 calls them
+v110_lookup <- ensembldb::select(ensembldbv110, 
+                                 keys = modern_mapping$GENEID, 
+                                 keytype = "GENEID", 
+                                 columns = c("GENEID", "SYMBOL"))%>%
+  distinct(SYMBOL, .keep_all = TRUE)
+
+
+dictionary <- left_join(v110_lookup,  modern_mapping, 
+          by = "GENEID") %>% 
+  left_join(gene_name_check, 
+            by = c("GENENAME"="Suggested.Symbol")) %>%
+  rename("original.name"= "x", "v110_name"= "SYMBOL.x")%>%
+  dplyr::select(c(GENEID, original.name, v110_name))
+
+known_fusions_ah <- left_join(known_fusions,  dictionary, 
+                                   by  = c("Gene1"="original.name")) %>% 
+  left_join(dictionary, by  = c("Gene2"="original.name"))
+write_tsv(known_fusions_ah, file="~/LongReadFusionCallerBenchmark/CellLineData/known_fusions_ah.tsv")
+####################################################################################
+# manually annotate where there are empty spaces by looking at ensembl information
+####################################################################################
+known_fusions_manual_annotation <- read_tsv("~/LongReadFusionCallerBenchmark/CellLineData/known_fusions_manual_annotation.tsv")
 
 #make annotation of list of known fusions
-known_fusions_biomart <-known_fusions_manual_annotation %>% left_join(gene_info, by  = c("ensembl_gene_id.x"="ensembl_gene_id")) %>% left_join(gene_info_2, by  = c("ensembl_gene_id.y"="ensembl_gene_id")) %>% select(c(1:6, 9, 12:13))%>% 
-  mutate(external_gene_name.y = coalesce(external_gene_name.y, Gene2), external_gene_name.x = coalesce(external_gene_name.x, Gene1), fusionGeneID = paste0(ensembl_gene_id.x, "::", ensembl_gene_id.y) , fusionGene = paste0(external_gene_name.x, "::", external_gene_name.y))
-known_fusions_biomart$discovery <- "known"
+known_fusions_manual_annotation <-known_fusions_manual_annotation %>% 
+  rename("ensembl_gene_id_1"="GENEID.x", "ensembl_gene_id_2"="GENEID.y") %>% 
+  dplyr::select(c(1:2, 6:9)) %>%
+ mutate(fusionGene = paste0(v110_name.x, "::", v110_name.y),
+        fusion.gene.id = paste0(ensembl_gene_id_1, "::",ensembl_gene_id_2))
+known_fusions_manual_annotation$discovery <- "known"
 
+#####################################
 #Depmap known fusions
+######################################
 DepMap_knownfusions <- read.csv("~/LibraryBenchmarkAnalysis/LibraryBenchmarkAnalysis_RProject/DepMap_OmicsFusionFiltered.csv", 
                                 header = TRUE)
 Model_Depmap <- (read.csv("~/LibraryBenchmarkAnalysis/LibraryBenchmarkAnalysis_RProject/Model.csv", 
