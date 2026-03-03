c8ae891e6be4f5475ddb877ffd42f2f678f10411
RDorney <ryley.dorney@my.jcu.edu.au>
2026-03-03 15:22:51 +1000

    updating where figures are stored and colour scheme

--- Figures/SCRIPTS/ReadSuppComp.R
+++ Figures/SCRIPTS/ReadSuppComp.R
@@ -12,7 +12,7 @@ Figure3B <-ggplot(filter(fusions_in_all_algorithms, depth == "100GB",
   theme_minimal() +
   facet_nested(~ recall_category , scales = "free_x", space = "free",
                labeller = as_labeller(c("Partial_Recall" = "Partial Recall", "True_Recall" = "True Recall")))+
-  scale_fill_gradient2(low = "lemonchiffon", mid="orange" ,high = "orchid", na.value = "grey", midpoint = 25)+
+  scale_fill_viridis()+
   scale_alpha_continuous(range = c(0.1, 1)) + 
   theme(strip.text.x = element_text(size = 12),
         legend.position = "right",
@@ -27,4 +27,4 @@ Figure3B <-ggplot(filter(fusions_in_all_algorithms, depth == "100GB",
         axis.text.x.bottom = element_text(size = 10),
         axis.title = element_text(size = 12))+ plot_layout(guides = 'keep')
 
-ggsave("~/LongReadFusionCallerBenchmark/Figures/Figure3B_paper.pdf", plot = Figure3B, width = 297, height = 210, units = "mm") 
+ggsave("~/LongReadFusionCallerBenchmark/Figures/FigureFiles/Figure3B_paper.pdf", plot = Figure3B, width = 297, height = 210, units = "mm") 
